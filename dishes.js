e876bb6932b0734f1eca9765ed13524a90d3e8fb
Aigul Baidavletova <aigulsobakuno@gmail.com>
2025-12-17 23:58:09 +0300

    Create dishes.js

---
+++ dishes.js
@@ -0,0 +1,78 @@
+
+const dishes = [
+    {
+        keyword: "borsch",
+        name: "Борщ",
+        price: 180,
+        category: "soup",
+        count: "300 грамм",
+        image: "борщ.jpg"
+    },
+    {
+        keyword: "mushroom-soup",
+        name: "Грибной крем-суп",
+        price: 180,
+        category: "soup",
+        count: "300 грамм",
+        image: "грибной.jpg"
+    },
+    {
+        keyword: "chicken-soup",
+        name: "Куриный суп",
+        price: 150,
+        category: "soup",
+        count: "300 грамм",
+        image: "куриный.jpg"
+    },
+    
+  
+    {
+        keyword: "beef-stroganoff",
+        name: "Бефстроганов с гречкой",
+        price: 250,
+        category: "main",
+        count: "250 грамм",
+        image: "бестро.jpg"
+    },
+    {
+        keyword: "carbonara",
+        name: "Карбонара",
+        price: 230,
+        category: "main",
+        count: "250 грамм",
+        image: "карбонара.jpg"
+    },
+    {
+        keyword: "chicken-cutlets",
+        name: "Куриные котлеты с пюре",
+        price: 185,
+        category: "main",
+        count: "300 грамм",
+        image: "контент.jpg"
+    },
+    
+    {
+        keyword: "tea",
+        name: "Чай в ассортименте",
+        price: 50,
+        category: "drink",
+        count: "200 мл",
+        image: "чай.jpg"
+    },
+    {
+        keyword: "milkshake",
+        name: "Молочный коктейль в ассортименте",
+        price: 200,
+        category: "drink",
+        count: "300 мл",
+        image: "мол кокте.jpg"
+    },
+    {
+        keyword: "lemonade",
+        name: "Лимонад в ассортименте",
+        price: 100,
+        category: "drink",
+        count: "300 мл",
+        image: "лимонады.jpg"
+    }
+];
